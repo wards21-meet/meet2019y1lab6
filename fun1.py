db53eb69052a8be5434cec7f8daeec4341793711
wards21-meet <wards21@meet.mit.edu>
2019-07-28 09:36:12 -0400

    Stopping here

---
+++ fun1.py
@@ -0,0 +1,7 @@
+
+total = 0
+for number in range(1, 10 + 1):
+    print(number)
+    total = total + number
+print(total)
+
---
+++ function.py
@@ -0,0 +1,9 @@
+def add_numbers():
+    total=0
+    for number in range(1, 10 + 1):
+        print(number)
+        total = total + number
+    return total
+    
+answer=add_numbers()
+print(answer)
